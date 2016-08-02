46cc07006ac82f4d73ceab8b9bceba369c906960
勇 <wangyong813111@163.com>
2016-08-02 16:55:31 +0800

    yo angular 生成初始化项目，npm install 安装所需依赖包，bower install 安装项目所需文件

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name deractiveApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the deractiveApp
+ */
+angular.module('deractiveApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
